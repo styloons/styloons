0a303466e766b7a616e8ed659b2bd4f63230c935
Samadhan <37985168+SamadhanIITkgp@users.noreply.github.com>
2024-03-21 16:35:11 +0530

    Update script.js
    Signed-off-by: Samadhan  <37985168+SamadhanIITkgp@users.noreply.github.com>

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ var menuBtn = document.getElementById("menuBtn")
         menuBtn.onclick = function () {
             if (sideNav.style.right == "-250px") {
                 sideNav.style.right = "0";
-                menu.src = "img/close.png";
+                menu.src = "close.png";
             }
             else {
                 sideNav.style.right = "-250px";
-                menu.src = "img/menu.png";
+                menu.src = "menu.png";
             }
         }
 
